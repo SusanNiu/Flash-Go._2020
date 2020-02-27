749cfde2dffa18e694d170398aa093c42be74083
YuanyuanNiu2019 <50037916+YuanyuanNiu2019@users.noreply.github.com>
2020-02-26 20:41:20 -0500

    update

--- app/src/main/java/com/example/flashgo/MainActivity.java
+++ app/src/main/java/com/example/flashgo/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.flashgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +16,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+        /*findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+        findViewById(R.id.flashcard_answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+            }
+        });/**/
+        findViewById(R.id.flashcard_answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+            }
+        });
+
+        findViewById(R.id.flashcard_answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+            }
+        });
+
+        findViewById(R.id.flashcard_answer4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer4).setBackgroundColor(getResources().getColor(R.color.my_green_color, null));
+            }
+        });
+
+       findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.toggle_choices_visibility).setImageResource(R.drawable.hide_icon);
             }
         });
     }
