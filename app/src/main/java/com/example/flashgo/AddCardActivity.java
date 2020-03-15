1dcad5a0695d6a968492ee7fa305ee93377b1303
YuanyuanNiu2019 <50037916+YuanyuanNiu2019@users.noreply.github.com>
2020-03-14 22:19:01 -0400

    update "indent"

--- app/src/main/java/com/example/flashgo/AddCardActivity.java
+++ app/src/main/java/com/example/flashgo/AddCardActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -20,12 +21,32 @@ public class AddCardActivity extends AppCompatActivity {
                 AddCardActivity.this.startActivity(intent);
             }
         });
-        findViewById(R.id.mySave).setOnClickListener(new View.OnClickListener() {
+        /*findViewById(R.id.mySave).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
                 AddCardActivity.this.startActivity(intent);
-
+            }
+        });*/
+        /*findViewById(R.id.mySave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+                AddCardActivity.this.startActivityForResult(intent,100);
+            }
+        });*/
+        findViewById(R.id.mySave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String somestring =((EditText)findViewById(R.id.editTextField)).getText().toString();
+                String anotherstring = ((EditText)findViewById(R.id.editAnswerField)).getText().toString();
+                Intent data = new Intent();
+                data.putExtra("string1", somestring);
+                data.putExtra("string2", anotherstring);
+                setResult(RESULT_OK, data);
+                finish();
             }
         });
-}}
+
+    }
+}
